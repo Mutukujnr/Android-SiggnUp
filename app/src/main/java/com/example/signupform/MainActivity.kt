801ffc812b4dfe54ec1a59322ce67672c1087444
amos <amosmwololo02@gmail.com>
2024-08-05 06:49:49 -0700

    added a login form with navigation

--- app/src/main/java/com/example/signupform/MainActivity.kt
+++ app/src/main/java/com/example/signupform/MainActivity.kt
@@ -3,8 +3,10 @@ package com.example.signupform
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -13,7 +15,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
@@ -29,10 +33,16 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.signupform.ui.theme.SignUpFormTheme
 
 class MainActivity : ComponentActivity() {
@@ -45,7 +55,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                  SignUpForm()
+                  MyApp()
                 }
             }
         }
@@ -53,11 +63,83 @@ class MainActivity : ComponentActivity() {
 }
 
 
-
+@Composable
+fun MyApp() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "sign_up") {
+        composable("sign_up") { SignUpForm(navController) }
+        composable("login") { LoginForm(navController) }
+    }
+}
 
 @Composable
-fun SignUpForm() {
-    var name by remember { mutableStateOf("") }
+fun LoginForm(navController: NavHostController) {
+
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFFEDEDED))
+    ){
+        Image(
+            painter = painterResource(id = R.drawable.login_bg),
+            contentDescription = "Background Image",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxSize()
+        )
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+                .background(Color.White.copy(alpha = 0.8f))
+                .padding(16.dp)
+                .verticalScroll(rememberScrollState()),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ){
+            Text("Login", fontSize = 30.sp, color = Color.Black)
+            Spacer(modifier = Modifier.height(16.dp))
+
+            InputTextField(label = R.string.email,
+                keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Email,
+                imeAction = ImeAction.Next
+            ),
+                value = email,
+                onValueChange = {email = it}
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+
+            InputTextField(label = R.string.password,
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Password,
+                    imeAction = ImeAction.Done
+                ),
+                value = password,
+                onValueChange = {password = it}
+            )
+            Spacer(modifier = Modifier.height(18.dp))
+            Button(onClick = { /* Handle sign-up logic */ }, modifier = Modifier.fillMaxWidth()) {
+                Text("Login")
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+            Text(
+                text = "Don't have an account? Sign Up",
+                color = Color.Blue,
+                modifier = Modifier.clickable {
+                    navController.navigate("sign_up")
+                }
+            )
+        }
+    }
+}
+
+@Composable
+fun SignUpForm(navController: NavHostController) {
+    var firstname by remember { mutableStateOf("") }
+    var lastname by remember { mutableStateOf("") }
     var email by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
     var confirmPassword by remember { mutableStateOf("") }
@@ -78,57 +160,102 @@ fun SignUpForm() {
                 .fillMaxSize()
                 .padding(16.dp)
                 .background(Color.White.copy(alpha = 0.8f))
-                .padding(16.dp),
+                .padding(16.dp)
+                .verticalScroll(rememberScrollState()),
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center
         ) {
             Text("Sign Up", fontSize = 30.sp, color = Color.Black)
             Spacer(modifier = Modifier.height(16.dp))
-            OutlinedTextField(
-                value = name,
-                onValueChange = { name = it },
-                label = { Text("Name") },
-                modifier = Modifier.fillMaxWidth()
+
+            InputTextField(
+                label = R.string.firstname,
+                value = firstname,
+                onValueChange = {firstname = it},
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Number,
+                    imeAction = ImeAction.Next
+                )
+            )
+
+
+            Spacer(modifier = Modifier.height(8.dp))
+            InputTextField(label = R.string.lastname,
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Text,
+                    imeAction = ImeAction.Next
+                ),
+                value = lastname,
+                onValueChange ={lastname = it}
             )
             Spacer(modifier = Modifier.height(8.dp))
-            OutlinedTextField(
-                value = email,
-                onValueChange = { email = it },
-                label = { Text("Email") },
-                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
-                modifier = Modifier.fillMaxWidth()
-            )
+           InputTextField(
+               label = R.string.email,
+               keyboardOptions = KeyboardOptions(
+                   keyboardType = KeyboardType.Email,
+                   imeAction = ImeAction.Next
+
+               ),
+               value = email,
+               onValueChange = {email = it}
+           )
             Spacer(modifier = Modifier.height(8.dp))
-            OutlinedTextField(
+            InputTextField(
+                label = R.string.password,
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Password,
+                    imeAction = ImeAction.Next
+                ),
                 value = password,
-                onValueChange = { password = it },
-                label = { Text("Password") },
-                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
-                modifier = Modifier.fillMaxWidth()
+                onValueChange = {password = it}
             )
             Spacer(modifier = Modifier.height(8.dp))
-            OutlinedTextField(
-                value = confirmPassword,
-                onValueChange = { confirmPassword = it },
-                label = { Text("Confirm Password") },
-                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
-                modifier = Modifier.fillMaxWidth()
-            )
+           InputTextField(label = R.string.confirm_passwrd,
+               keyboardOptions = KeyboardOptions(
+                   keyboardType = KeyboardType.Password,
+                   imeAction = ImeAction.Done
+               ),
+               value = confirmPassword,
+               onValueChange = {confirmPassword = it}
+           )
             Spacer(modifier = Modifier.height(18.dp))
             Button(onClick = { /* Handle sign-up logic */ }, modifier = Modifier.fillMaxWidth()) {
                 Text("Sign Up")
             }
+            Spacer(modifier = Modifier.height(16.dp))
+            Text(
+                text = "Already have an account? Log in",
+                color = Color.Blue,
+                modifier = Modifier.clickable {
+                    navController.navigate("login")
+                }
+            )
         }
     }
 }
 
-
+@Composable
+fun InputTextField(
+    @StringRes label: Int,
+    keyboardOptions: KeyboardOptions,
+    value: String,
+    onValueChange: (String) -> Unit,
+    modifier: Modifier = Modifier
+){
+    OutlinedTextField(
+        value = value,
+        onValueChange = onValueChange,
+        label = { Text(stringResource(label))},
+        keyboardOptions = keyboardOptions,
+        modifier = Modifier.fillMaxWidth()
+    )
+}
 
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     SignUpFormTheme {
-       SignUpForm()
+       MyApp()
     }
 }
